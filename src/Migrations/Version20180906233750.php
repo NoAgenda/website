6cd293683b6bb36f0425accbbb1a664efc1ce616
Tim Goudriaan <tim@codedmonkey.com>
2018-09-08 21:59:21 +0200

    Fix latest migration

--- src/Migrations/Version20180906233750.php
+++ src/Migrations/Version20180906233750.php
@@ -15,7 +15,7 @@ final class Version20180906233750 extends AbstractMigration
         $this->addSql('ALTER TABLE na_user ADD activation_token_expires_at DATETIME DEFAULT NULL COMMENT \'(DC2Type:datetime_immutable)\'');
         $this->addSql('ALTER TABLE na_user ADD created_at DATETIME DEFAULT NULL COMMENT \'(DC2Type:datetime_immutable)\'');
         $this->addSql('UPDATE na_user SET created_at = "2018-09-01 00:00:00"');
-        $this->addSql('ALTER TABLE na_user ADD created_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\'');
+        $this->addSql('ALTER TABLE na_user CHANGE created_at created_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\'');
     }
 
     public function down(Schema $schema): void
