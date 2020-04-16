3a5cfc2f799d8cb9cfc214a20bfb5865d7f2a016
Tim Goudriaan <tim@codedmonkey.com>
2020-04-17 01:50:30 +0200

    Remove whitelist in favor of session

--- public/index.php
+++ public/index.php
@@ -6,18 +6,20 @@ use Symfony\Component\HttpFoundation\Request;
 
 require dirname(__DIR__) . '/config/bootstrap.php';
 
-if ($_SERVER['APP_DEBUG']) {
-    $ip = [
-        '127.0.0.1',
-        '172.18.0.1',
-        '82.173.117.56',
-    ];
+if ($_SERVER['APP_ENV'] === 'staging') {
+    session_start();
 
-    if (!in_array($_SERVER['REMOTE_ADDR'], $ip)) {
-        header('HTTP/1.1 403 Forbidden');
-        die('Not allowed: ' . $_SERVER['REMOTE_ADDR']);
+    if (isset($_GET['itm'])) {
+        $_SESSION['itm'] = true;
     }
 
+    if (!isset($_SESSION['itm'])) {
+        header('HTTP/1.1 403 Forbidden');
+        die('Not allowed');
+    }
+}
+
+if ($_SERVER['APP_DEBUG']) {
     umask(0000);
 
     Debug::enable();
