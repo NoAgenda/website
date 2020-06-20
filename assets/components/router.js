8041f56276bc94e2fa4f1f9faca981a306cca48e
Tim Goudriaan <tim@codedmonkey.com>
2020-06-21 01:07:23 +0200

    Add bootleg Mastodon button

--- assets/components/router.js
+++ assets/components/router.js
@@ -1,6 +1,7 @@
 import jQuery from 'jquery';
 
 import Archive from '../scripts/archive';
+import {initializeMastodonButtons} from '../scripts/mastodon';
 import {getPlayer} from '../scripts/player';
 import tokenManager from '../scripts/token';
 
@@ -111,6 +112,9 @@ class RouterElement extends HTMLElement {
     if (archiveElement) {
       this.archive = new Archive();
     }
+
+    // Mastodon buttons
+    initializeMastodonButtons();
 
     // Chrome notice
     const isChrome = /chrome/.test(window.navigator.userAgent.toLowerCase());
