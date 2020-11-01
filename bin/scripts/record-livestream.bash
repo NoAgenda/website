43c5f4f95610a5247e94cf88b803a25db91ba281
Tim Goudriaan <tim@codedmonkey.com>
2020-11-01 19:51:34 +0100

    Update CS of Bash-scripts

--- bin/scripts/record-livestream.bash
+++ bin/scripts/record-livestream.bash
@@ -1,12 +1,11 @@
 #!/usr/bin/env bash
 
-if [ $# -eq 0 ]
-  then
-    echo "Supply a relative path for the recording. The generated files will have an extension appended to it."
-    exit 1
+if [ $# -eq 0 ]; then
+  echo "Supply a relative path for the recording. The generated files will have an extension appended to it."
+  exit 1
 fi
 
 PATH=$1
-echo "Recording $PATH.asf ...";
-/usr/bin/mplayer "http://listen.noagendastream.com/noagenda" -dumpstream -dumpfile $PATH.asf &> $PATH.log & /bin/sleep 60s;
-/usr/bin/pkill mplayer;
+echo "Recording $PATH.asf ..."
+mplayer "http://listen.noagendastream.com/noagenda" -dumpstream -dumpfile $PATH.asf &> $PATH.log & sleep 60s
+pkill mplayer
