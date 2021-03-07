72d0581f6ef91bfb096bf46cc0eb1b6b460370c2
Tim Goudriaan <tim@codedmonkey.com>
2021-03-07 15:00:45 +0100

    Add API action to trigger feed crawler

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Episode;
 use App\Message\CrawlEpisodeTranscript;
+use App\Message\CrawlFeed;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Messenger\MessageBusInterface;
@@ -22,11 +23,25 @@ class ApiController extends AbstractController
     }
 
     /**
+     * @Route("/crawl_feed/{token}", name="api_crawl_feed")
+     */
+    public function crawlFeed(string $token): Response
+    {
+        if ($token !== $_SERVER['API_SECURITY_TOKEN']) {
+            return new Response('Invalid token: ' . $token, 400);
+        }
+
+        $this->messenger->dispatch(new CrawlFeed());
+
+        return new Response('OK');
+    }
+
+    /**
      * @Route("/crawl_transcripts/{token}/{episode}", name="api_crawl_transcripts", defaults={"episode"=""})
      */
     public function crawlTranscripts(string $episode, string $token): Response
     {
-        if ($token !== $_SERVER['API_TRANSCRIPT_TOKEN']) {
+        if ($token !== $_SERVER['API_SECURITY_TOKEN']) {
             return new Response('Invalid token: ' . $token, 400);
         }
 
