71a86c095b4cfbd843d6aa34357f2c3354dcb3fe
Tim Goudriaan <tim@codedmonkey.com>
2021-10-27 13:57:57 +0200

    Add more common exceptions to ignore list for Sentry

--- src/EventListener/Sentry/BeforeSendListener.php
+++ src/EventListener/Sentry/BeforeSendListener.php
@@ -5,15 +5,22 @@ namespace App\EventListener\Sentry;
 use Sentry\Event;
 use Sentry\EventHint;
 use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 
 class BeforeSendListener
 {
     private $ignoredExceptions = [
+        // http
         AccessDeniedHttpException::class,
+        BadRequestHttpException::class,
         MethodNotAllowedHttpException::class,
         NotFoundHttpException::class,
+
+        // security
+        AccessDeniedException::class,
     ];
 
     public function __invoke(Event $event, EventHint $hint): ?Event
