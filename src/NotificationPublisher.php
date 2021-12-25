83d8822f56dd947b11820231d5618c67240c9a5a
Tim Goudriaan <tim@codedmonkey.com>
2021-12-26 00:25:20 +0100

    Refactor trigger to publish notifications

--- src/NotificationPublisher.php
+++ src/NotificationPublisher.php
@@ -24,7 +24,7 @@ class NotificationPublisher
 
     public function publishEpisode(Episode $episode)
     {
-        if ('prod' !== $_SERVER['APP_ENV']) {
+        if (false === (bool) $_SERVER['MASTODON_PUBLISH'] || '' === $_SERVER['MASTODON_ACCESS_TOKEN']) {
             return;
         }
 
@@ -34,45 +34,43 @@ class NotificationPublisher
 
         $title = sprintf('No Agenda Episode %s - %s', $code, $episode->getName());
 
-        if ($_SERVER['MASTODON_ACCESS_TOKEN']) {
-            /*
-            The following code is a failed attempt at adding the episode cover to the Mastodon toot.
-            I think it's because it collides with metadata for the link to the episode player.
-            It's still here for reference purposes because it should work when not including a link in the toot.
+        /*
+        The following code is a failed attempt at adding the episode cover to the Mastodon toot.
+        I think it's because it collides with metadata for the link to the episode player.
+        It's still here for reference purposes because it should work when not including a link in the toot.
 
-            $filename = "${code}.png";
-            $coverPath = sprintf('%s/episode_covers/%s.png', $_SERVER['APP_STORAGE_PATH'], $code);
+        $filename = "${code}.png";
+        $coverPath = sprintf('%s/episode_covers/%s.png', $_SERVER['APP_STORAGE_PATH'], $code);
 
-            $streamFactory = Psr17FactoryDiscovery::findStreamFactory();
-            $builder = new MultipartStreamBuilder($streamFactory);
-            $builder
-                ->addResource('file', file_get_contents($coverPath), ['filename' => $filename])
-                ->addResource('description', sprintf('Artwork for No Agenda episode %s', $code))
-            ;
+        $streamFactory = Psr17FactoryDiscovery::findStreamFactory();
+        $builder = new MultipartStreamBuilder($streamFactory);
+        $builder
+            ->addResource('file', file_get_contents($coverPath), ['filename' => $filename])
+            ->addResource('description', sprintf('Artwork for No Agenda episode %s', $code))
+        ;
 
-            $multipartStream = $builder->build();
+        $multipartStream = $builder->build();
 
-            $mediaResponse = $this->httpClient->post('/media', [
-                'Content-Type' => sprintf('multipart/form-data; boundary="%s"', $builder->getBoundary()),
-            ], $multipartStream);
+        $mediaResponse = $this->httpClient->post('/media', [
+            'Content-Type' => sprintf('multipart/form-data; boundary="%s"', $builder->getBoundary()),
+        ], $multipartStream);
 
-            if ($mediaResponse->getStatusCode() > 200) {
-                throw new \Exception('Failed to upload media to Mastodon.');
-            }
+        if ($mediaResponse->getStatusCode() > 200) {
+            throw new \Exception('Failed to upload media to Mastodon.');
+        }
 
-            $mediaData = json_decode($mediaResponse->getBody()->getContents(), true);
-            */
+        $mediaData = json_decode($mediaResponse->getBody()->getContents(), true);
+        */
 
-            $messageParameters = [
-                'status' => "$title $path",
-                // 'media_ids' => [$mediaData['id']],
-            ];
+        $messageParameters = [
+            'status' => "$title $path",
+            // 'media_ids' => [$mediaData['id']],
+        ];
 
-            $messageResponse = $this->httpClient->post('/statuses', [], http_build_query($messageParameters));
+        $messageResponse = $this->httpClient->post('/statuses', [], http_build_query($messageParameters));
 
-            if ($messageResponse->getStatusCode() > 200) {
-                throw new \Exception('Failed to upload episode notification to Mastodon.');
-            }
+        if ($messageResponse->getStatusCode() > 200) {
+            throw new \Exception('Failed to upload episode notification to Mastodon.');
         }
     }
 }
