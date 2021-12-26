f871383e97bee63fabfa54504151fdedc0a6fb96
Tim Goudriaan <tim@codedmonkey.com>
2021-12-26 21:04:36 +0100

    Fix code style in entrypoint files

--- docker/assets-entrypoint.sh
+++ docker/assets-entrypoint.sh
@@ -3,12 +3,12 @@
 set -e
 
 if [ "$1" = 'node' ] || [ "$1" = 'npm' ]; then
-	npm install
+  npm install
 
-	>&2 echo "Waiting for app to be ready..."
-	until nc -z ${FPM_HOST} ${FPM_PORT}; do
-		sleep 1
-	done
+  >&2 echo "Waiting for app to be ready..."
+  until nc -z ${FPM_HOST} ${FPM_PORT}; do
+    sleep 1
+  done
 fi
 
 exec "$@"
--- docker/php-entrypoint.bash
+++ docker/php-entrypoint.bash
@@ -3,10 +3,10 @@
 set -e
 
 if [ "$1" = 'bin/console' ]; then
-	>&2 echo "Waiting for app to be ready..."
-	until nc -z ${FPM_HOST} ${FPM_PORT}; do
-		sleep 1
-	done
+  >&2 echo "Waiting for app to be ready..."
+  until nc -z ${FPM_HOST} ${FPM_PORT}; do
+    sleep 1
+  done
 fi
 
 exec docker-php-entrypoint "$@"
