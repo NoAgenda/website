2acfbdbb77c8d2f0ef8a5f717f673e9b9c4fac7e
Tim Goudriaan <tim@codedmonkey.com>
2022-08-17 20:15:45 +0200

    Improve the algorithm for removing old files

--- src/Command/ClearDataCommand.php
+++ src/Command/ClearDataCommand.php
@@ -2,6 +2,8 @@
 
 namespace App\Command;
 
+use App\Entity\Episode;
+use App\Repository\EpisodeRepository;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -16,26 +18,60 @@ class ClearDataCommand extends Command
     protected static $defaultName = 'clear-data';
     protected static $defaultDescription = 'Clears old crawling data';
 
+    public function __construct(
+        private readonly EpisodeRepository $episodeRepository,
+    ) {
+        parent::__construct();
+    }
+
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $style = new SymfonyStyle($input, $output);
 
-        $this->clearDirectory($style, 'chat_logs');
-        $this->clearDirectory($style, 'livestream_recordings');
+        $files = [...$this->findOldChatLogs(), ...$this->findOldLivestreamRecordings()];
+        $this->removeFiles($files, $style);
 
         $style->success('Old crawling data has been removed.');
 
         return Command::SUCCESS;
     }
 
-    private function clearDirectory(OutputStyle $style, string $path): void
+    private function findOldChatLogs(): array
+    {
+        $recordingDates = array_filter(array_map(function (Episode $episode) {
+            return (int) $episode->getRecordedAt()->format('Ymd');
+        }, $this->episodeRepository->findAll()));
+
+        $recordedAfter = (int) (new \DateTime())->sub(new \DateInterval('P3D'))->format('Ymd');
+
+        $files = (new Finder())
+            ->files()
+            ->in($_SERVER['APP_STORAGE_PATH'].'/chat_logs')
+            ->filter(function(\SplFileInfo $file) use ($recordedAfter, $recordingDates) {
+                $recordingDate = (int) $file->getFilenameWithoutExtension();
+
+                if (in_array($recordingDate, $recordingDates)) {
+                    return false;
+                }
+
+                return $recordingDate < $recordedAfter;
+            });
+
+        return iterator_to_array($files);
+    }
+
+    private function findOldLivestreamRecordings(): array
     {
         $files = (new Finder())
             ->files()
-            ->date('< now - 28 days')
-            ->in(implode('/', [$_SERVER['APP_STORAGE_PATH'], $path]))
-        ;
+            ->in($_SERVER['APP_STORAGE_PATH'].'/livestream_recordings')
+            ->date('< now - 2 days');
 
+        return iterator_to_array($files);
+    }
+
+    private function removeFiles(array $files, OutputStyle $style): void
+    {
         $filesystem = new Filesystem();
 
         /** @var SplFileInfo $file */
@@ -45,6 +81,6 @@ class ClearDataCommand extends Command
             $filesystem->remove($file->getPathname());
         }
 
-        $style->success(sprintf('Removed %s files', count($files)));
+        $style->success(sprintf('Files removed: %s', count($files)));
     }
 }
