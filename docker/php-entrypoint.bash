836cec5a27a50c7d5c866eddffc4b68d4074a246
Tim Goudriaan <tim@codedmonkey.com>
2023-04-25 23:42:13 +0200

    Improve logging in PHP entrypoint

--- docker/php-entrypoint.bash
+++ docker/php-entrypoint.bash
@@ -3,10 +3,23 @@
 set -e
 
 if [ "$1" = 'bin/console' ]; then
-  >&2 echo "Waiting for app to be ready..."
-  until nc -z ${FPM_HOST} ${FPM_PORT}; do
-    sleep 1
+  echo "Waiting for app to be ready..."
+
+  ATTEMPTS_LEFT_TO_REACH_APPLICATION=60
+  until [ $ATTEMPTS_LEFT_TO_REACH_APPLICATION -eq 0 ] || nc -z ${FPM_HOST} ${FPM_PORT}; do
+    sleep 5
+
+    ATTEMPTS_LEFT_TO_REACH_APPLICATION=$((ATTEMPTS_LEFT_TO_REACH_APPLICATION - 1))
+    echo "Still waiting for app to be ready... Is it reachable? $ATTEMPTS_LEFT_TO_REACH_APPLICATION attempts left"
   done
+
+  if [ $ATTEMPTS_LEFT_TO_REACH_APPLICATION -eq 0 ]; then
+    echo "The app is not up or not reachable"
+
+    exit 1
+  else
+    echo "The app is now ready and reachable"
+  fi
 fi
 
 exec docker-php-entrypoint "$@"
