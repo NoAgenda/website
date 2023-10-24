cb7bc257725740f09bc341fa5cb6437bf59c3abf
Tim Goudriaan <tim@codedmonkey.com>
2023-10-24 16:53:13 +0200

    Add new episodes to fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -49,7 +49,9 @@ class AppFixtures extends Fixture
                 ->setShownotesPath($data['shownotesPath'])
                 ->setTranscriptUri($data['transcriptUri'])
                 ->setTranscriptPath($data['transcriptPath'])
-                ->setChatArchivePath($data['chatArchivePath']);
+                ->setChatArchivePath($data['chatArchivePath'] ?? null)
+                ->setChaptersUri($data['chaptersUri'] ?? null)
+                ->setChaptersPath($data['chaptersPath'] ?? null);
 
             $manager->persist($episode);
         }
@@ -145,6 +147,78 @@ class AppFixtures extends Fixture
             'transcriptUri' => 'https://www.noagendashow.net/media/transcripts/NA-1243-2020-05-17-Final.srt',
             'transcriptPath' => '/srv/app/docker/storage/transcripts/1243.srt',
             'chatArchivePath' => '/srv/app/docker/storage/chat_archives/1243.json',
+        ];
+
+        yield [
+            'code' => '1598',
+            'name' => 'Guardrails',
+            'author' => 'Adam Curry & John C. Dvorak',
+            'publishedAt' => '2023-10-12',
+            'duration' => 10246,
+            'recordingUri' => 'https://mp3s.nashownotes.com/NA-1598-2023-10-12-Final.mp3',
+            'coverUri' => 'https://noagendaassets.com/enc/1697145462.234_na-1598-art-feed.jpg',
+            'coverPath' => '/srv/app/docker/storage/covers/1598.png',
+            'publicShownotesUri' => 'http://adam.curry.com/html/NoAgendaEpisode1598G-F1JvMfM7fC3P2fLs1tjRQPTGtVBzdH.html',
+            'shownotesUri' => 'http://adam.curry.com/opml/NoAgendaEpisode1598G-F1JvMfM7fC3P2fLs1tjRQPTGtVBzdH.opml',
+            'shownotesPath' => '/srv/app/docker/storage/shownotes/1598.xml',
+            'transcriptUri' => 'https://mp3s.nashownotes.com/NA-1598-Captions.srt',
+            'transcriptPath' => '/srv/app/docker/storage/transcripts/1598.srt',
+            'chaptersUri' => 'https://chapters.hypercatcher.com/http:feed.nashownotes.comrss.xml/http:1598.noagendanotes.com',
+            'chaptersPath' => '/srv/app/docker/storage/chapters/1598.json',
+        ];
+
+        yield [
+            'code' => '1599',
+            'name' => 'Drop the Op',
+            'author' => 'Adam Curry & John C. Dvorak',
+            'publishedAt' => '2023-10-15',
+            'duration' => 11245,
+            'recordingUri' => 'https://mp3s.nashownotes.com/NA-1599-2023-10-15-Final.mp3',
+            'coverUri' => 'https://noagendaassets.com/enc/1697405313.159_na-1599-art-feed.jpg',
+            'coverPath' => '/srv/app/docker/storage/covers/1599.png',
+            'publicShownotesUri' => 'http://adam.curry.com/html/NoAgendaEpisode1599D-FC3bRtKpGHQbhw2gRSpJ9THr6T8P4g.html',
+            'shownotesUri' => 'http://adam.curry.com/opml/NoAgendaEpisode1599D-FC3bRtKpGHQbhw2gRSpJ9THr6T8P4g.opml',
+            'shownotesPath' => '/srv/app/docker/storage/shownotes/1599.xml',
+            'transcriptUri' => 'https://mp3s.nashownotes.com/NA-1599-Captions.srt',
+            'transcriptPath' => '/srv/app/docker/storage/transcripts/1599.srt',
+            'chaptersUri' => 'https://chapters.hypercatcher.com/http:feed.nashownotes.comrss.xml/http:1599.noagendanotes.com',
+            'chaptersPath' => '/srv/app/docker/storage/chapters/1599.json',
+        ];
+
+        yield [
+            'code' => '1600',
+            'name' => 'Unpack It',
+            'author' => 'Adam Curry & John C. Dvorak',
+            'publishedAt' => '2023-10-19',
+            'duration' => 13117,
+            'recordingUri' => 'https://mp3s.nashownotes.com/NA-1600-2023-10-19-Final.mp3',
+            'coverUri' => 'https://noagendaassets.com/enc/1697753376.704_na-1600-art-feed.jpg',
+            'coverPath' => '/srv/app/docker/storage/covers/1600.png',
+            'publicShownotesUri' => 'http://adam.curry.com/html/NoAgendaEpisode1600U-rm9njDsr92wSxFKqwgrX29LZnhTxM7.html',
+            'shownotesUri' => 'http://adam.curry.com/opml/NoAgendaEpisode1600U-rm9njDsr92wSxFKqwgrX29LZnhTxM7.opml',
+            'shownotesPath' => '/srv/app/docker/storage/shownotes/1600.xml',
+            'transcriptUri' => 'https://mp3s.nashownotes.com/NA-1600-Captions.srt',
+            'transcriptPath' => '/srv/app/docker/storage/transcripts/1600.srt',
+            'chaptersUri' => 'https://chapters.hypercatcher.com/http:feed.nashownotes.comrss.xml/http:1600.noagendanotes.com',
+            'chaptersPath' => '/srv/app/docker/storage/chapters/1600.json',
+        ];
+
+        yield [
+            'code' => '1601',
+            'name' => 'Unkool & The Gang',
+            'author' => 'Adam Curry & John C. Dvorak',
+            'publishedAt' => '2023-10-22',
+            'duration' => 11958,
+            'recordingUri' => 'https://mp3s.nashownotes.com/NA-1601-2023-10-22-Final.mp3',
+            'coverUri' => 'https://noagendaassets.com/enc/1698011763.8_na-1601-art-feed.jpg',
+            'coverPath' => '/srv/app/docker/storage/covers/1601.png',
+            'publicShownotesUri' => 'http://adam.curry.com/html/NoAgendaEpisode1601U-mRMHnxjHgmwrG00jpGZpMnp1FDFMdT.html',
+            'shownotesUri' => 'http://adam.curry.com/opml/NoAgendaEpisode1601U-mRMHnxjHgmwrG00jpGZpMnp1FDFMdT.opml',
+            'shownotesPath' => '/srv/app/docker/storage/shownotes/1601.xml',
+            'transcriptUri' => 'https://mp3s.nashownotes.com/NA-1601-Captions.srt',
+            'transcriptPath' => '/srv/app/docker/storage/transcripts/1601.srt',
+            'chaptersUri' => 'https://chapters.hypercatcher.com/http:feed.nashownotes.comrss.xml/http:1601.noagendanotes.com',
+            'chaptersPath' => '/srv/app/docker/storage/chapters/1601.json',
         ];
     }
 
