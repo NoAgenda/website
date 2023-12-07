31ca7cd09695060db51769cb208bdaeac2b6db9f
Tim Goudriaan <tim@codedmonkey.com>
2023-12-07 11:05:15 +0100

    Refactor storage path in fixtures #72

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -14,6 +14,8 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
+        $storagePath = $_SERVER['APP_STORAGE_PATH'];
+
         foreach ($this->loadUsers() as list($username, $email, $plainPassword, $roles)) {
             $user = (new User())
                 ->setReviewed(true);
@@ -43,15 +45,15 @@ class AppFixtures extends Fixture
                 ->setDuration($data['duration'])
                 ->setRecordingUri($data['recordingUri'])
                 ->setCoverUri($data['coverUri'])
-                ->setCoverPath($data['coverPath'])
+                ->setCoverPath($storagePath . '/' . $data['coverPath'])
                 ->setPublicShownotesUri($data['publicShownotesUri'])
                 ->setShownotesUri($data['shownotesUri'])
-                ->setShownotesPath($data['shownotesPath'])
+                ->setShownotesPath($storagePath . '/' . $data['shownotesPath'])
                 ->setTranscriptUri($data['transcriptUri'])
-                ->setTranscriptPath($data['transcriptPath'])
-                ->setChatArchivePath($data['chatArchivePath'] ?? null)
+                ->setTranscriptPath($storagePath . '/' . $data['transcriptPath'])
+                ->setChatArchivePath($data['chatArchivePath'] ? $storagePath . '/' . $data['chatArchivePath'] : null)
                 ->setChaptersUri($data['chaptersUri'] ?? null)
-                ->setChaptersPath($data['chaptersPath'] ?? null);
+                ->setChaptersPath($data['chaptersPath'] ? $storagePath . '/' . $data['chaptersPath'] : null);
 
             $manager->persist($episode);
         }
@@ -89,13 +91,13 @@ class AppFixtures extends Fixture
             'duration' => 11010,
             'recordingUri' => 'https://mp3s.nashownotes.com/NA-1240-2020-05-07-Final.mp3',
             'coverUri' => 'https://noagendaassets.com/enc/1588880905.893_na-1240-art-feed.png',
-            'coverPath' => '/srv/app/docker/storage/covers/1240.png',
+            'coverPath' => 'covers/1240.png',
             'publicShownotesUri' => 'http://adam.curry.com/html/NoAgendaEpisode1240M-F99GP9GcZq67jjGxnmDW106bKW3pr4.html',
             'shownotesUri' => 'http://adam.curry.com/opml/NoAgendaEpisode1240M-F99GP9GcZq67jjGxnmDW106bKW3pr4.opml',
-            'shownotesPath' => '/srv/app/docker/storage/shownotes/1240.xml',
+            'shownotesPath' => 'shownotes/1240.xml',
             'transcriptUri' => 'https://www.noagendashow.net/media/transcripts/NA-1240-2020-05-07-Final.srt',
-            'transcriptPath' => '/srv/app/docker/storage/transcripts/1240.srt',
-            'chatArchivePath' => '/srv/app/docker/storage/chat_archives/1240.json',
+            'transcriptPath' => 'transcripts/1240.srt',
+            'chatArchivePath' => 'chat_archives/1240.json',
         ];
 
         yield [
@@ -106,13 +108,13 @@ class AppFixtures extends Fixture
             'duration' => 12507,
             'recordingUri' => 'https://mp3s.nashownotes.com/NA-1241-2020-05-10-Final.mp3',
             'coverUri' => 'https://noagendaassets.com/enc/1589141237.285_na-1241-art-feed.png',
-            'coverPath' => '/srv/app/docker/storage/covers/1241.png',
+            'coverPath' => 'covers/1241.png',
             'publicShownotesUri' => 'http://adam.curry.com/html/NoAgendaEpisode1241C-XqBF4nm8G2R4WRK2hHftm0Pw1PBrgg.html',
             'shownotesUri' => 'http://adam.curry.com/opml/NoAgendaEpisode1241C-XqBF4nm8G2R4WRK2hHftm0Pw1PBrgg.opml',
-            'shownotesPath' => '/srv/app/docker/storage/shownotes/1241.xml',
+            'shownotesPath' => 'shownotes/1241.xml',
             'transcriptUri' => 'https://www.noagendashow.net/media/transcripts/NA-1241-2020-05-10-Final.srt',
-            'transcriptPath' => '/srv/app/docker/storage/transcripts/1241.srt',
-            'chatArchivePath' => '/srv/app/docker/storage/chat_archives/1241.json',
+            'transcriptPath' => 'transcripts/1241.srt',
+            'chatArchivePath' => 'chat_archives/1241.json',
         ];
 
         yield [
@@ -123,13 +125,13 @@ class AppFixtures extends Fixture
             'duration' => 11790,
             'recordingUri' => 'https://mp3s.nashownotes.com/NA-1242-2020-05-14-Final.mp3',
             'coverUri' => 'https://noagendaassets.com/enc/1589486598.491_na-1242-art-feed.png',
-            'coverPath' => '/srv/app/docker/storage/covers/1242.png',
+            'coverPath' => 'covers/1242.png',
             'publicShownotesUri' => 'http://adam.curry.com/html/NoAgendaEpisode1242S-dHMsL7fj385gtjDwVNn7422WDQblG4.html',
             'shownotesUri' => 'http://adam.curry.com/opml/NoAgendaEpisode1242S-dHMsL7fj385gtjDwVNn7422WDQblG4.opml',
-            'shownotesPath' => '/srv/app/docker/storage/shownotes/1242.xml',
+            'shownotesPath' => 'shownotes/1242.xml',
             'transcriptUri' => 'https://www.noagendashow.net/media/transcripts/NA-1242-2020-05-14-Final.srt',
-            'transcriptPath' => '/srv/app/docker/storage/transcripts/1242.srt',
-            'chatArchivePath' => '/srv/app/docker/storage/chat_archives/1242.json',
+            'transcriptPath' => 'transcripts/1242.srt',
+            'chatArchivePath' => 'chat_archives/1242.json',
         ];
 
         yield [
@@ -140,13 +142,13 @@ class AppFixtures extends Fixture
             'duration' => 11085,
             'recordingUri' => 'https://mp3s.nashownotes.com/NA-1243-2020-05-17-Final.mp3',
             'coverUri' => 'https://noagendaassets.com/enc/1589744564.723_na-1243-art-feed.png',
-            'coverPath' => '/srv/app/docker/storage/covers/1243.png',
+            'coverPath' => 'covers/1243.png',
             'publicShownotesUri' => 'http://adam.curry.com/html/NoAgendaEpisode1243O-XVFr4Sf7Jhh9ddD5WsZQM3C70ZCgZb.html',
             'shownotesUri' => 'http://adam.curry.com/opml/NoAgendaEpisode1243O-XVFr4Sf7Jhh9ddD5WsZQM3C70ZCgZb.opml',
-            'shownotesPath' => '/srv/app/docker/storage/shownotes/1243.xml',
+            'shownotesPath' => 'shownotes/1243.xml',
             'transcriptUri' => 'https://www.noagendashow.net/media/transcripts/NA-1243-2020-05-17-Final.srt',
-            'transcriptPath' => '/srv/app/docker/storage/transcripts/1243.srt',
-            'chatArchivePath' => '/srv/app/docker/storage/chat_archives/1243.json',
+            'transcriptPath' => 'transcripts/1243.srt',
+            'chatArchivePath' => 'chat_archives/1243.json',
         ];
 
         yield [
@@ -157,14 +159,14 @@ class AppFixtures extends Fixture
             'duration' => 10246,
             'recordingUri' => 'https://mp3s.nashownotes.com/NA-1598-2023-10-12-Final.mp3',
             'coverUri' => 'https://noagendaassets.com/enc/1697145462.234_na-1598-art-feed.jpg',
-            'coverPath' => '/srv/app/docker/storage/covers/1598.png',
+            'coverPath' => 'covers/1598.png',
             'publicShownotesUri' => 'http://adam.curry.com/html/NoAgendaEpisode1598G-F1JvMfM7fC3P2fLs1tjRQPTGtVBzdH.html',
             'shownotesUri' => 'http://adam.curry.com/opml/NoAgendaEpisode1598G-F1JvMfM7fC3P2fLs1tjRQPTGtVBzdH.opml',
-            'shownotesPath' => '/srv/app/docker/storage/shownotes/1598.xml',
+            'shownotesPath' => 'shownotes/1598.xml',
             'transcriptUri' => 'https://mp3s.nashownotes.com/NA-1598-Captions.srt',
-            'transcriptPath' => '/srv/app/docker/storage/transcripts/1598.srt',
+            'transcriptPath' => 'transcripts/1598.srt',
             'chaptersUri' => 'https://chapters.hypercatcher.com/http:feed.nashownotes.comrss.xml/http:1598.noagendanotes.com',
-            'chaptersPath' => '/srv/app/docker/storage/chapters/1598.json',
+            'chaptersPath' => 'chapters/1598.json',
         ];
 
         yield [
@@ -175,14 +177,14 @@ class AppFixtures extends Fixture
             'duration' => 11245,
             'recordingUri' => 'https://mp3s.nashownotes.com/NA-1599-2023-10-15-Final.mp3',
             'coverUri' => 'https://noagendaassets.com/enc/1697405313.159_na-1599-art-feed.jpg',
-            'coverPath' => '/srv/app/docker/storage/covers/1599.png',
+            'coverPath' => 'covers/1599.png',
             'publicShownotesUri' => 'http://adam.curry.com/html/NoAgendaEpisode1599D-FC3bRtKpGHQbhw2gRSpJ9THr6T8P4g.html',
             'shownotesUri' => 'http://adam.curry.com/opml/NoAgendaEpisode1599D-FC3bRtKpGHQbhw2gRSpJ9THr6T8P4g.opml',
-            'shownotesPath' => '/srv/app/docker/storage/shownotes/1599.xml',
+            'shownotesPath' => 'shownotes/1599.xml',
             'transcriptUri' => 'https://mp3s.nashownotes.com/NA-1599-Captions.srt',
-            'transcriptPath' => '/srv/app/docker/storage/transcripts/1599.srt',
+            'transcriptPath' => 'transcripts/1599.srt',
             'chaptersUri' => 'https://chapters.hypercatcher.com/http:feed.nashownotes.comrss.xml/http:1599.noagendanotes.com',
-            'chaptersPath' => '/srv/app/docker/storage/chapters/1599.json',
+            'chaptersPath' => 'chapters/1599.json',
         ];
 
         yield [
@@ -193,14 +195,14 @@ class AppFixtures extends Fixture
             'duration' => 13117,
             'recordingUri' => 'https://mp3s.nashownotes.com/NA-1600-2023-10-19-Final.mp3',
             'coverUri' => 'https://noagendaassets.com/enc/1697753376.704_na-1600-art-feed.jpg',
-            'coverPath' => '/srv/app/docker/storage/covers/1600.png',
+            'coverPath' => 'covers/1600.png',
             'publicShownotesUri' => 'http://adam.curry.com/html/NoAgendaEpisode1600U-rm9njDsr92wSxFKqwgrX29LZnhTxM7.html',
             'shownotesUri' => 'http://adam.curry.com/opml/NoAgendaEpisode1600U-rm9njDsr92wSxFKqwgrX29LZnhTxM7.opml',
-            'shownotesPath' => '/srv/app/docker/storage/shownotes/1600.xml',
+            'shownotesPath' => 'shownotes/1600.xml',
             'transcriptUri' => 'https://mp3s.nashownotes.com/NA-1600-Captions.srt',
-            'transcriptPath' => '/srv/app/docker/storage/transcripts/1600.srt',
+            'transcriptPath' => 'transcripts/1600.srt',
             'chaptersUri' => 'https://chapters.hypercatcher.com/http:feed.nashownotes.comrss.xml/http:1600.noagendanotes.com',
-            'chaptersPath' => '/srv/app/docker/storage/chapters/1600.json',
+            'chaptersPath' => 'chapters/1600.json',
         ];
 
         yield [
@@ -211,14 +213,14 @@ class AppFixtures extends Fixture
             'duration' => 11958,
             'recordingUri' => 'https://mp3s.nashownotes.com/NA-1601-2023-10-22-Final.mp3',
             'coverUri' => 'https://noagendaassets.com/enc/1698011763.8_na-1601-art-feed.jpg',
-            'coverPath' => '/srv/app/docker/storage/covers/1601.png',
+            'coverPath' => 'covers/1601.png',
             'publicShownotesUri' => 'http://adam.curry.com/html/NoAgendaEpisode1601U-mRMHnxjHgmwrG00jpGZpMnp1FDFMdT.html',
             'shownotesUri' => 'http://adam.curry.com/opml/NoAgendaEpisode1601U-mRMHnxjHgmwrG00jpGZpMnp1FDFMdT.opml',
-            'shownotesPath' => '/srv/app/docker/storage/shownotes/1601.xml',
+            'shownotesPath' => 'shownotes/1601.xml',
             'transcriptUri' => 'https://mp3s.nashownotes.com/NA-1601-Captions.srt',
-            'transcriptPath' => '/srv/app/docker/storage/transcripts/1601.srt',
+            'transcriptPath' => 'transcripts/1601.srt',
             'chaptersUri' => 'https://chapters.hypercatcher.com/http:feed.nashownotes.comrss.xml/http:1601.noagendanotes.com',
-            'chaptersPath' => '/srv/app/docker/storage/chapters/1601.json',
+            'chaptersPath' => 'chapters/1601.json',
         ];
     }
 
